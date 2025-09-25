3d789a1943f67f2429c42941597c47c1485346cc
Guillaume Roy <222312274+GRoy-CEOLeap@users.noreply.github.com>
2025-09-25 11:08:48 -0400

    Update logo size to 300x99px

--- src/custom/components/ClockworkHero.tsx
+++ src/custom/components/ClockworkHero.tsx
@@ -31,7 +31,7 @@ export const ClockworkHero: React.FC = () => {
               <img 
                 src="/logo__2_-removebg-preview.png" 
                 alt="Clockwork Funding Logo" 
-                className="w-[150px] h-[150px]"
+                className="w-[300px] h-[99px]"
               />
             </div>
             
